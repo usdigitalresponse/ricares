8ac0b9c80847664deacfc0e8603f104eabe4c6d0
V David Zvenyach <dave@esq.io>
2020-04-25 10:56:49 -0500

    ready for netlify

--- src/templates/report.js
+++ src/templates/report.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
-import printJS from "print-js";
+const printJS = typeof window !== `undefined` ? require("print-js") : null;
 
 export default ({ data }) => {
   const report = data.airtable.data;
